cf94d159a10e8beac1e63410c4de5eb4245b7554
Yunus Kara <ynus.kara6116@gmail.com>
2020-05-11 01:19:13 +0300

    Add project files.

---
+++ ProjeforVize.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream file("input.txt");
+    string str;
+    while (getline(file, str)) {
+        cout << str << "\n";
+    }
+
+
+
+
+    ofstream outfile("output.txt");
+    ofstream outfile("turns.txt");
+    outfile << "my text here!" <<endl;
+
+    outfile.close();
+}
